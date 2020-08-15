9de6d6b1eb0f25bcccf449521b214d2d30502775
AmoghLonkar <alonkar@ucsc.edu>
2020-08-15 07:56:54 -0700

    Implementation of Laxman's Paper

---
+++ src/rec_count.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <string>
+#include <set>
+#include <omp.h>
+#include <unordered_map>
+#include "benchmark.h"
+#include "builder.h"
+#include "command_line.h"
+#include "graph.h"
+#include "pvector.h"
+#include "platform_atomics.h"
+
+using namespace std;
+
+int RecCount(Graph &g, vector<NodeID> I, int l){
+	if(l == 1){
+		return I.size();
+	}
+}
+
+int main(int argc, char* argv[]){
+	CLBase cli(argc, argv, "subgraph isomorphism");
+	if (!cli.ParseArgs()){
+		return -1;
+	}
+
+	Builder b(cli);
+	Graph g = b.MakeGraph();
+
+	int k = atoi(argv[3]);
+
+	return 0;
+}	
