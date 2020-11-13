4c1d01ad95cf486043a2153ab01b2b84a75fed79
AmoghLonkar <alonkar@ucsc.edu>
2020-11-12 16:34:25 -0800

    Initialized subgraph, need to finish building subgraph

--- src/kclist.cc
+++ src/kclist.cc
@@ -43,6 +43,42 @@ void Init(Graph &g, Graph_Info *g_i, int k){
 	g_i->sub = sub;	
 }
 
+void Listing(Graph &g, Graph_Info *g_i, int l, int *n){
+	if(l == 2){
+		for(int i = 0; i < g_i->ns[2]; i++){
+			(*n) += g_i->d[2][i];
+		}	
+	}
+	
+	// For each node in g_l
+	// Initializing vertex-induced subgraph
+	for(int i = 0; i < g_i->ns[l]; i++){
+		
+		g_i->ns[l-1] = 0;
+		for(NodeID neighbor: g.out_neigh(i)){
+			if(g_i->lab[neighbor] == l){
+				g_i->lab[neighbor] = l-1;
+				g_i->sub[l-1][g_i->ns[l-1]++] = i;
+			}
+			
+		}	
+	}
+	
+	// Building subgraph
+	for(int i = 0; i < g_i->ns[l-1]; i++){
+		
+	}	
+
+	Listing(g, g_i, l-1, n);
+
+	//Resetting labels	
+	for(int i = 0; i < g_i->ns[l-1]; i++){
+		int node = g_i->sub[l-1][i];
+		g_i->lab[node] = l;
+	}
+
+}
+
 int main(int argc, char* argv[]){
 	CLBase cli(argc, argv, "subgraph isomorphism");
 	if (!cli.ParseArgs()){
@@ -55,18 +91,16 @@ int main(int argc, char* argv[]){
 	int k = atoi(argv[3]);
 
 	Graph_Info graph_struct;
-	Graph_Info *g_i = &graph_struct; 
+	
 	auto start = std::chrono::system_clock::now();
-	Init(dag, g_i, k);
+	Init(dag, &graph_struct, k);
 	auto end = std::chrono::system_clock::now();
 	auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-
-	for(int i = 0; i < dag.num_nodes(); i++){
-		cout << g_i->sub[k][i] << endl;
-	}
 	cout << "Time to create graph struct: " << elapsed.count() << endl; 
 	
-	//cout << "Number of cliques: " << count << endl;
+	int n = 0;
+	//Listing(dag, &graph_struct, k, &n);
+	cout << "Number of cliques: " << n << endl;
 	cout << "Time to calculate possible subgraph isomorphisms: " <<elapsed.count() << "s" << endl;
 	return 0;
 }
