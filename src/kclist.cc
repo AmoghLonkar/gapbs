6d0496b2a3c3ca5553dcbedf0338460252aa78f9
AmoghLonkar <alonkar@ucsc.edu>
2020-11-13 14:24:46 -0800

    Works for triangles, need to re-order nodes in subgraph according to decreasing degree

--- src/kclist.cc
+++ src/kclist.cc
@@ -56,21 +56,18 @@ void Listing(Graph &g, Graph_Info *g_i, int l, int *n){
 	// Initializing vertex-induced subgraph
 	for(int i = 0; i < g_i->ns[l]; i++){
 		g_i->ns[l-1] = 0;
-		//Adding root node to subgraph
-		//g_i->sub[l-1][g_i->ns[l-1]++] = g_i->sub[l][i];
-		
 
 		for(NodeID neighbor: g.out_neigh(i)){
 			if(g_i->lab[neighbor] == l){
 				g_i->lab[neighbor] = l-1;
 				
-				//Adding nodes to subgraph
+				// Adding nodes to subgraph
 				g_i->sub[l-1][g_i->ns[l-1]++] = neighbor;
 			}
 			
 		}
 		
-		//Only proceed if there is potential for a clique
+		// Only proceed if there is potential for a clique
 		if(g_i->ns[l-1] >= l - 1){
 		//{
 			// Building subgraph
@@ -85,14 +82,14 @@ void Listing(Graph &g, Graph_Info *g_i, int l, int *n){
 						(g_i->d[l-1][j])++;
 					}
 				}
+			}
 
-			cout << "Node " << g_i->sub[l-1][j] << ", Degree: " << g_i->d[l-1][j] << endl;	
-			}	
-			cout << "Next recursion level" << endl;	
-			Listing(g, g_i, l-1, n);
+			// Nodes in decreasing order of degree	
 		}
 		
-		//Resetting labels	
+		Listing(g, g_i, l-1, n);
+		
+		// Resetting labels	
 		for(int k = 0; k < g_i->ns[l-1]; k++){
 			int node = g_i->sub[l-1][k];
 			g_i->lab[node] = l;
